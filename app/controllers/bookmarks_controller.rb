df548c5fd98e353627eb79d751d3342cf070b960
anais meirone <anais.meirone@live.fr>
2021-08-12 17:59:55 +0200

    bookmark almost finished need destroy action

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+  def new
+    # we need @list in our `simple_form_for`
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to @list
+    else
+      render 'new'
+    end
+  end
+
+  # def destroy
+  # end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  # /lists/:id
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_list
+    params.require(:list).permit(:name)
+  end
+end
