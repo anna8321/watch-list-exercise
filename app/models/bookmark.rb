378b9f4363f2b0394d0469c19939de26aa5f65f3
anais meirone <anais.meirone@live.fr>
2021-08-13 11:17:49 +0200

    correct error

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   validates :comment, presence: true, length: { minimum: 6 }
-  validates :list, uniqueness: true
+  validates :list, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'is already in the list' }
   belongs_to :movie
   belongs_to :list
 end
